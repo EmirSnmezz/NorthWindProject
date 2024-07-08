a516fe777318c47b88e542b6797992b22b90ce32
emir <emircan_snmez@outlook.com>
2024-07-08 17:32:27 +0300

    project and layers has been created of northwind project

---
+++ Core/Utilities/Results/Concrete/DataResult/SuccessDataResult.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results.Concrete.DataResult
+{
+    public class SuccessDataResult<T> /* :IDataResult<T> where T : class, IEntity, new();*/
+    {
+    }
+}
---
+++ Core/Utilities/Results/Concrete/Result/ErrorResult.cs
@@ -0,0 +1,15 @@
+﻿using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results.Concrete.Result
+{
+    public class ErrorResult : IResult
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
